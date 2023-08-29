e7af9951c040edbc7648700887640ac0ac206906
Andres Riascos <popoeto@gmail.com>
2023-08-29 16:12:40 -0500

    clase 29 agosto 2023

---
+++ node-curso.00/03-http/1.http.js
@@ -0,0 +1,30 @@
+const http = require("node:http");
+const fs = require("node:fs");
+
+const server = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (req.url === "/") {
+      res.end("<h1>Hola esta es mi primera página!</h1>");
+    } else if (req.url === "/contacto") {
+      res.end("<h2>Mi telefono es 12345678</h2>");
+    } else if (req.url === "/usuario") {
+        res.end("<h1>Hola Soy Andres Riascos</h1>");
+    } else if (req.url === "/imagen") {
+      fs.readFile("./imagen-01.JPEG", (err, data) => {
+        if (err){
+          res.statusCode = 500; //error interno del server
+          res.end("<h1> Error interno del servidor </h1>");
+        } else {
+          res.setHeader("Content-Type", "image/JPEG")
+          res.end(data);
+        }
+      })
+  } else {
+      res.statusCode = 404; // not found
+      res.end("<p>Recurso no encontrado - 404</p>");
+    }
+  });
+  
+  server.listen(3000, () => {
+    console.log("Servidor escuchando en el puerto: http://localhost:3000");
+  });
